1bd0f814a03ac5f12dc60c03ee58caf2dfec7cf9
veljkovicm <61838091+veljkovicm@users.noreply.github.com>
2020-06-18 14:36:29 +0200

    Update database.js

--- util/database.js
+++ util/database.js
@@ -1,15 +1,3 @@
-// const mysql = require('mysql2');
-
-// const pool = mysql.createPool({
-//   host: 'localhost',
-//   user: 'root',
-//   database: 'node-complete',
-//   password: 'Loktar321'
-// });
-
-// module.exports = pool.promise();
-
-
 const Sequelize = require('sequelize/index');
 
 const sequelize = new Sequelize('<DB_NAME>', '<USERNAME>', '<PASSWORD>', {

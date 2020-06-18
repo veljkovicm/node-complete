4b1a1be91511c6398163076ccb4d2dda944bf622
Milan <comiv92@gmail.com>
2020-06-18 15:31:28 +0200

    Adds order models

---
+++ models/orderItem.js
@@ -0,0 +1,18 @@
+const Sequelize = require('sequelize/index');
+
+const sequelize = require('../util/database');
+
+const OrderItem = sequelize.define('orderItem', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  }
+});
+
+module.exports = OrderItem;
+
+
+
+

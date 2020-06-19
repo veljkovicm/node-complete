cf8c4f315ee2408884ede45957210c0f54ab5d30
Milan <comiv92@gmail.com>
2020-06-19 14:54:48 +0200

    WIP: Replace Sequelize with MongoDB

--- util/database.example.js
+++ util/database.example.js
@@ -1,8 +1,14 @@
-const Sequelize = require('sequelize/index');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('<DB_NAME>', '<USERNAME>', '<PASSWORD>', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const mongoConnect = (callback) => {
+MongoClient
+  .connect('mongodb+srv://<user>:<password>@cluster0-o3skn.mongodb.net/<dbname>?retryWrites=true&w=majority')
+  .then(client => {
+    console.log('Connected!');
+    callback(client);
+  })
+  .catch(err => console.log(err));
+}
 
-module.exports = sequelize;
+module.exports = mongoConnect;

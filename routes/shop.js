59b6850bb2ee12024d1e7d00390edfc709deb0fe
Milan <comiv92@gmail.com>
2020-06-19 13:57:07 +0200

    Resetting the cart & Fetching and Outputting orders

--- routes/shop.js
+++ routes/shop.js
@@ -22,7 +22,6 @@ router.post('/create-order', shopController.postOrder);
 
 router.get('/orders', shopController.getOrders);
 
-router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
 

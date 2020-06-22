445f7122bea1f76399c98dde5bf199ab34ef6d39
Milan <comiv92@gmail.com>
2020-06-22 17:02:05 +0200

    Add user model

--- models/product.js
+++ models/product.js
@@ -21,7 +21,7 @@ class Product {
     }
     return dbOp
       .then(result => {
-        // console.log(result)
+        console.log(result)
       })
       .catch(err => console.log(err));
   };

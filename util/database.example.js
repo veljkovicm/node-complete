aad59883adab9975c5e799be362d19321b5a719f
Milan <comiv92@gmail.com>
2020-06-22 10:30:06 +0200

    Update the db connection file

--- util/database.example.js
+++ util/database.example.js
@@ -5,7 +5,7 @@ let _db;
 
 const mongoConnect = (callback) => {
 MongoClient
-  .connect('mongodb+srv://<user>:<password>@cluster0-o3skn.mongodb.net/<dbname>?retryWrites=true&w=majority')
+  .connect('mongodb+srv://<user>:<password>@cluster0-qnj2p.mongodb.net/Cluster0?retryWrites=true&w=majority')
   .then(client => {
     console.log('Connected!');
     _db = client.db();

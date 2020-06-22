124fafb0dfe4b3f9a3fff730d3847ef5f69648b6
Milan <comiv92@gmail.com>
2020-06-22 12:26:24 +0200

    Update the EditProduct methods to work with MongoDB

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,7 @@
+const mongodb = require('mongodb');
 const Product = require('../models/product');
+
+const ObjectId = mongodb.ObjectId;
 
 exports.getAddProduct = (req, res, next) => { // ommit next if not using
   res.render('admin/edit-product', {
@@ -26,49 +29,43 @@ exports.postAddProduct = (req, res, next) => {
     });
 };
 
-// exports.getEditProduct = (req, res, next) => { // ommit next if not using
-//   const editMode = req.query.edit;
-//   if(!editMode) {
-//     res.redirect('/');
-//   }
-//   const prodId = req.params.productId;
-//   req.user
-//     .getProducts({where: {id: prodId}})
-//     .then(products => {
-//       const product = products[0];
-//       if (!product) {
-//         return res.redirect('');
-//       }
-//       res.render('admin/edit-product', {
-//         pageTitle: 'Edit Product',
-//         path: '/admin/edit-product',
-//         editing: editMode,
-//         product: product,
-//       });
-//     })
-//     .catch(err => console.log(err));
-// }
+exports.getEditProduct = (req, res, next) => { // ommit next if not using
+  const editMode = req.query.edit;
+  if(!editMode) {
+    res.redirect('/');
+  }
+  const prodId = req.params.productId;
+  Product.findById(prodId)
+    .then(product => {
+      if (!product) {
+        return res.redirect('');
+      }
+      res.render('admin/edit-product', {
+        pageTitle: 'Edit Product',
+        path: '/admin/edit-product',
+        editing: editMode,
+        product: product,
+      });
+    })
+    .catch(err => console.log(err));
+}
 
-// exports.postEditProduct = (req, res, next) => {
-//   const prodId = req.body.productId;
-//   const updatedTitle = req.body.title;
-//   const updatedPrice = req.body.price;
-//   const updatedImageUrl = req.body.imageUrl;
-//   const updatedDesc = req.body.description;
-//   Product.findByPk(prodId)
-//     .then(product => {
-//       product.title = updatedTitle;
-//       product.price = updatedPrice;
-//       product.description = updatedDesc;
-//       product.imageUrl = updatedImageUrl;
-//       return product.save();
-//     })
-//     .then(result => {
-//       console.log('UPDATED PRODUCT')
-//       res.redirect('/admin/products');
-//     })
-//     .catch(err => console.log(err))
-// }
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDesc = req.body.description;
+  
+  const product = new Product(updatedTitle, updatedPrice, updatedDesc, updatedImageUrl, new ObjectId(prodId))
+  product
+  .save()
+  .then(result => {
+    console.log('UPDATED PRODUCT')
+    res.redirect('/admin/products');
+  })
+  .catch(err => console.log(err))
+}
 
 // exports.postDeleteProduct = (req, res, next) => {
 //   const prodId = req.body.productId;

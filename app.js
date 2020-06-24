53136b37608f1e1db553df6e2d92110aa941bf4c
Milan <comiv92@gmail.com>
2020-06-24 14:50:15 +0200

    Removes old cart models; Update addToCart function to work with MongoDB

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('5ef0c72c518da2ffd82f96d4')
   .then(user => {
-    req.user = user;
+    req.user = new User(user.name, user.email, user.cart, user._id);
     next();
   })
   .catch(err => console.log(err));

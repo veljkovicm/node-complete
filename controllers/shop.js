ec9ae8a77c870afe56443dacc791f55693327fc3
Milan <comiv92@gmail.com>
2020-06-25 15:23:03 +0200

    update the getCart method to work with Mongoose

--- controllers/shop.js
+++ controllers/shop.js
@@ -49,13 +49,15 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   req.user
-    .getCart()
-    .then(products => {
-        res.render('shop/cart', {
-          path: '/cart',
-          pageTitle: 'Your cart',
-          products: products,
-        });
+    .populate('cart.items.productId', )
+    .execPopulate()
+    .then(user => {
+      const products = user.cart.items;
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your cart',
+        products: products,
+      });
       })
     .catch(err => console.log(err));
 };

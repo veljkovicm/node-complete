e8ee8523a2a32769389fde4d24e5f7cb2a04fee8
Milan <comiv92@gmail.com>
2020-06-25 12:37:45 +0200

    Adds relational order data

--- models/user.js
+++ models/user.js
@@ -77,18 +77,31 @@ class User {
 
   addOrder() {
     const db = getDb();
-    return db
-      .collection('orders')
-      .insertOne(this.cart)
-      .then(result => {
-        this.cart = {items: []};
-        return db
-        .collection('users')
-        .updateOne(
-          { _id: ObjectId(this._id) },
-          { $set: { cart: {items: [] } } }
-        );
-      })
+    return this.getCart()
+    .then(products => {
+      const order = {
+        items: products,
+        user: {
+          _id: new ObjectId(this._id),
+          name: this.name
+        }
+      };
+      return db.collection('orders').insertOne(order)
+    })
+    .then(result => {
+      this.cart = { items: [] };
+      return db
+      .collection('users')
+      .updateOne(
+        { _id: ObjectId(this._id) },
+        { $set: { cart: {items: [] } } }
+      );
+    })
+  }
+
+  getOrders() {
+    const db = getDb();
+    // return db.collection('orders')
   }
 
   static findById(userId) {

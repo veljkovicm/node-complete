d04b20053c71c143c9545e8c9ba887096fd02626
Milan <comiv92@gmail.com>
2020-06-30 14:37:41 +0200

    Adds password validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check, validationResult } = require('express-validator/check');
+const { check, body } = require('express-validator/check');
 
 const authController = require('../controllers/auth');
 
@@ -11,15 +11,20 @@ router.post('/login', authController.postLogin);
 router.get('/signup', authController.getSignup);
 router.post(
   '/signup',
-  check('email')
-    .isEmail()
-    .withMessage('Please enter a valid email.')
-    .custom((value, {req}) => {
-      if (value === 'test@test.com') {
-        throw new Error('This email address is forbidden');
-      }
-      return true
-    }),
+  [
+    check('email')
+      .isEmail()
+      .withMessage('Please enter a valid email.')
+      .custom((value, {req}) => {
+        if (value === 'test@test.com') {
+          throw new Error('This email address is forbidden');
+        }
+        return true
+      }),
+    body('password', 'Invalid password')
+      .isLength({min: 5, max: 10})
+      .isAlphanumeric()
+  ],
   authController.postSignup,
 );
 

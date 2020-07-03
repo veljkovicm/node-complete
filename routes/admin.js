35c5323bfac246bd55543c89992cd3feaeda569f
Milan <comiv92@gmail.com>
2020-07-03 17:15:42 +0200

    Updates delete method to use client side js

--- routes/admin.js
+++ routes/admin.js
@@ -49,7 +49,7 @@ router.post(
   isAuth,
   adminController.postEditProduct);
 
-router.post('/delete-product', isAuth, adminController.postDeleteProduct);
+router.delete('/product/:productId', isAuth, adminController.deleteProduct);
 
 router.post('/dummy', isAuth, adminController.addDummyProduct);
 

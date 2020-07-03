beddae1892b2e1e570035d191ef4135df1d427a9
Milan <comiv92@gmail.com>
2020-07-03 18:30:29 +0200

    removes app.js from gitignore;moves db key to config file

--- config/config.default.js
+++ config/config.default.js
@@ -2,4 +2,5 @@ module.exports = {
   sendGridKey: '<SENDGRID_API_KEY_HERE>',
   sendGridEmail: 'milanemcr@gmail.com',
   stripePrivateKey: '<STRIPE_API_KEY_HERE>',
+  MONGODB_URI: '<MONGODB_URI_KEY_HERE>',
 }
